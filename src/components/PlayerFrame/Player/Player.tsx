f9dd0827e5834dc8efd113886d264af3ae6fea8a
seifert.pascal@web.de <seifert.pascal@web.de>
2023-01-03 16:27:42 +0100

    feat: added colormatrix for each colorblindness

--- src/components/PlayerFrame/Player/Player.tsx
+++ src/components/PlayerFrame/Player/Player.tsx
@@ -55,9 +55,14 @@ function Player({
      */
   };
 
+
+   /**
+   * Video-Rendering Section
+   */
+  
   interface MetaData {
     videoWidth: HTMLVideoElement["videoWidth"],
-    videoHeight:HTMLVideoElement["videoHeight"],
+    videoHeight: HTMLVideoElement["videoHeight"],
   }
 
   let video: HTMLVideoElement;
@@ -96,29 +101,97 @@ function Player({
     });
   }
 
+
+  /**
+   * Colorblind Simulation Section
+  */
+
+  interface ColorMatrix {
+    red: number[],
+    green: number[],
+    blue: number[],
+  }
+
+  const protanopia = {
+    red: [0.567, 0.433, 0],
+    green: [0.558, 0.442, 0],
+    blue: [0, 0.242, 0.758],
+  }
+
+  const protanomaly = { // Severity = 1
+    red: [0.152286, 1.052583, -0.204868],
+    green: [0.114503, 0.786281, 0.099216],
+    blue: [-0.003882, -0.048116, 1.051998],
+  }
+
+  const deuteranopia = {
+    red: [0.625, 0.375, 0],
+    green: [0.7, 0.3, 0],
+    blue: [0, 0.3, 0.7],
+  }
+
+  const tritanopia = {
+    red: [0.95, 0.05, 0],
+    green: [0, 0.433, 0.567],
+    blue: [0, 0.475, 0.525],
+  }
+
+  const getColorMatrixForBlindness = (blindness: string): ColorMatrix => {
+    switch(blindness) {
+      case "Protanopia":
+        return protanopia;
+      case "Protanomaly":
+        return protanomaly;
+      case "Deuteranopia":
+        return deuteranopia;
+      case "Tritanopia":
+        return tritanopia;
+      default:
+        return { red: [1,0,0], green: [0,1,0], blue: [0,0,1] }
+    }
+  }
+
+
+  const applytestEffectSepia = (imageData: ImageData): void => {
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+      let red = data[i],
+        green = data[i + 1],
+        blue = data[i + 2];
+
+      data[i] = Math.min(
+        Math.round(0.393 * red + 0.769 * green + 0.189 * blue),
+        255
+      );
+      data[i + 1] = Math.min(
+        Math.round(0.349 * red + 0.686 * green + 0.168 * blue),
+        255
+      );
+      data[i + 2] = Math.min(
+        Math.round(0.272 * red + 0.534 * green + 0.131 * blue),
+        255
+      );
+    }
+    ctx.putImageData(imageData, 0, 0);
+  };
+
   //TODO fix resize artefakt am anfang 
 
   const applyColorFilter = (): void => {
     ctx.drawImage(video, 0, 0, colorBlindCanvas.width, colorBlindCanvas.height);
     const imageData = ctx.getImageData(0, 0, colorBlindCanvas.width, colorBlindCanvas.height);
-    const data = imageData.data;
-    for (let i = 0; i < data.length; i += 4) {
-        let red = data[i],
-            green = data[i + 1],
-            blue = data[i + 2];
 
-        data[i] = Math.min(
-            Math.round(0.393 * red + 0.769 * green + 0.189 * blue),
-            255
-        );
-        data[i + 1] = Math.min(
-            Math.round(0.349 * red + 0.686 * green + 0.168 * blue),
-            255
-        );
-        data[i + 2] = Math.min(
-            Math.round(0.272 * red + 0.534 * green + 0.131 * blue),
-            255
-        );
+    const data = imageData.data;
+    const colormatrix = getColorMatrixForBlindness("Protanopia"); //Should be coming from the 'blindess selector' component
+    
+    for (let i = 0; i < data.length; i += 4) {
+      let red = data[i],
+        green = data[i + 1],
+        blue = data[i + 2];
+
+      data[i] = Math.round(colormatrix.red[0] * red + colormatrix.red[1] * green + colormatrix.red[2] * blue);
+      data[i + 1] = Math.round(colormatrix.green[0] * red + colormatrix.green[1] * green + colormatrix.green[2] * blue);
+      data[i + 2] = Math.round(colormatrix.blue[0] * red + colormatrix.blue[1] * green + colormatrix.blue[2] * blue);
     }
     ctx.putImageData(imageData, 0, 0);
   }
