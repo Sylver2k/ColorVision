27b9397d30964ba400cc7a2617cad416935b62ce
seifert.pascal@web.de <seifert.pascal@web.de>
2023-01-27 12:18:12 +0100

    feat: remove test effect (sepia)

--- src/components/PlayerFrame/Player/Player.tsx
+++ src/components/PlayerFrame/Player/Player.tsx
@@ -213,29 +213,6 @@ function Player({
     }
   };
 
-  const applytestEffectSepia = (imageData: ImageData): void => {
-    const data = imageData.data;
-    for (let i = 0; i < data.length; i += 4) {
-      let red = data[i],
-        green = data[i + 1],
-        blue = data[i + 2];
-
-      data[i] = Math.min(
-        Math.round(0.393 * red + 0.769 * green + 0.189 * blue),
-        255
-      );
-      data[i + 1] = Math.min(
-        Math.round(0.349 * red + 0.686 * green + 0.168 * blue),
-        255
-      );
-      data[i + 2] = Math.min(
-        Math.round(0.272 * red + 0.534 * green + 0.131 * blue),
-        255
-      );
-    }
-    ctx.putImageData(imageData, 0, 0);
-  };
-
   //TODO fix resize artefakt am anfang
 
   const applyColorFilter = (): void => {
